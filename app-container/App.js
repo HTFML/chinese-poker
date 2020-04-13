8420a431ab7ba316b4ecd05e84c0f225b2de6f30
github username <github email address>
2020-04-13 14:47:38 -0400

    react-navigation 5.0

--- app-container/App.js
+++ app-container/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import PlayScreen from './src/components/tabscreens/PlayScreen.js';
 import DeliveryScreen from './src/components/tabscreens/DeliveryScreen.js';
 import HomeScreen from './src/components/tabscreens/HomeScreen.js';
 import RewardsScreen from './src/components/tabscreens/RewardsScreen.js';
 
-const TabNavigator = createBottomTabNavigator(
-  {
-    Home: HomeScreen,
-    Play: PlayScreen,
-    Delivery: DeliveryScreen,
-    Rewards: RewardsScreen
-  });
+// const TabNavigator = createBottomTabNavigator(
+//   {
+//     Home: HomeScreen,
+//     Play: PlayScreen,
+//     Delivery: DeliveryScreen,
+//     Rewards: RewardsScreen
+//   });
 
-export default createAppContainer(TabNavigator);
+// export default createAppContainer(TabNavigator);
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
+}
+
+
+export default App;

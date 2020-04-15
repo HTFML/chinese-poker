43094ec391ab422eb21c5ddc91a82352ff857bcc
John Speck <speckworks@gmail.com>
2020-04-14 22:27:02 -0400

    added some line breaks and changed fontWeight added Rules button to Home

--- Big2/App.js
+++ Big2/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/components/tabscreens/HomeScreen.js';
-import RulesScreen from './src/components/tabscreens/Rules.js'
+import RulesScreen from './src/components/tabscreens/Rules.js';
+import { Text, Button, View} from 'react-native';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
+
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Home"
         rulesRoute="Rules"
-        screenOptions={{ gestureEnabled: true }}
+        screenOptions={{ gestureEnabled: false }}
       >
         <Stack.Screen
           name="Rules"
@@ -23,7 +25,13 @@ const App = () => {
           component={HomeScreen} 
         />
       </Stack.Navigator>
+    <View>
+    <Button title="Rules" onPress={() => navigation.navigate('Rules')}>
+    Rules
+    </Button>
+    </View>
     </NavigationContainer>
+
     )
 }
 

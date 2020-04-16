22b875963f3e5e599e66fc93fe0a510ea4cef81c
Ali <ali1195shah@gmail.com>
2020-04-16 19:10:50 -0400

    playtest2

--- Big2/src/components/tabscreens/Lobby.js
+++ Big2/src/components/tabscreens/Lobby.js
@@ -1,9 +1,14 @@
 import React, {useState} from 'react'
-import {View, StyleSheet, Image, TextInput, TouchableHighlight, Text} from 'react-native'
+import {View, StyleSheet, Image, TextInput, TouchableHighlight, Text, Button, Alert} from 'react-native'
+import Modal from 'react-native-modal';
+
 
 export default function Lobby() {
     
-    const [userName, setUserName] = useState('')
+  const [isModalVisible, setModal] = useState(false)
+  const [userName, setUserName] = useState('')
+
+  const handleNo = () => {}
 
     return (
         
@@ -11,17 +16,39 @@ export default function Lobby() {
                 <View style={styles.inputContainer}>
                     <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' }} />
                     <TextInput style={styles.inputs}
-                        placeholder="user name"
+                        placeholder="Enter Your Username"
                         keyboardType="email-address"
                         underlineColorAndroid='transparent'
                         onChangeText={(userName) => setUserName({ userName })} />
+                        
                 </View>
-
   
+                <Button title="Next" onPress={() => setModal(true)} />
+        <Modal isVisible={isModalVisible} style={{backgroundColor: 'white', margin:60, marginVertical: 360, borderRadius: 20}}>
+          <View style={{justifyContent: 'center', alignItems: 'center', margin: 0}} >
+            <Text>Are you over 21?
+            </Text>
+            
+            <View style={{flexDirection: 'row'}}>
+              <Button title="Yes"/>
+              <Button title="No" onPress={() => setModal(false)} />
+            </View>
+          </View>
+        </Modal>
 
-                <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => onClickListener('login')}>
-                    <Text style={styles.loginText}>Play</Text>
-                </TouchableHighlight>
+        
+          
+
+                {/* <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => setModal(false)}>
+                <Modal isVisible={this.state.isModalVisible}>
+                  <View style={{flex: 1}}>
+                    <Text>Hello!</Text>
+                    <Button title="Hide modal" onPress={this.toggleModal} />
+                  </View>
+                  </Modal>
+                    <Text style={styles.loginText}>Next</Text>
+                </TouchableHighlight> */}
+
                 {/* <TouchableHighlight style={[styles.buttonContainer, styles.ageButton]} onPress={() => onClickListener('login')}>
                     <Text style={styles.loginText}>Are you under 18 years old?</Text>
                     <Button></Button>
@@ -41,6 +68,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#DCDCDC',
     },
     inputContainer: {
+        
         borderBottomColor: '#F5FCFF',
         backgroundColor: '#FFFFFF',
         borderRadius:30,
@@ -53,7 +81,8 @@ const styles = StyleSheet.create({
     },
     inputs:{
         height:45,
-        marginLeft:16,
+        textAlign: "center",
+        marginLeft:-30,
         borderBottomColor: '#FFFFFF',
         flex:1,
     },

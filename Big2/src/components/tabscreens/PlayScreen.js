11c68c9eb8e594e3cef2096f2bf0439f757f08f0
Ali <ali1195shah@gmail.com>
2020-04-16 20:10:10 -0400

    age val is working

--- Big2/src/components/tabscreens/PlayScreen.js
+++ Big2/src/components/tabscreens/PlayScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const PlayScreen = () => {
+const PlayScreen = ({ route }) => {
+  const { userName } = route.params
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Play!</Text>
+      <Text>{userName}</Text>
     </View>
   )
 }

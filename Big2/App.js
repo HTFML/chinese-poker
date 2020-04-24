6247ac1e847a97793cf2069d9bf03bbcbc6101bf
github username <github email address>
2020-04-24 18:17:29 -0400

    rebase finished

--- Big2/App.js
+++ Big2/App.js
@@ -3,8 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen.js';
 import PlayScreen from './src/screens/PlayScreen.js';
-import Rules from './src/screens/Rules.js'
-import Login from './src/screens/Login.js'
+import Rules from './src/screens/Rules.js';
+import Login from './src/screens/Login.js';
+import Lobby from './src/screens/Lobby.js';
 
 const Stack = createStackNavigator();
 
@@ -28,6 +29,10 @@ const App = () => {
         <Stack.Screen 
           name="Play"
           component={PlayScreen}
+        />
+        <Stack.Screen 
+          name="Lobby"
+          component={Lobby}
         />
         <Stack.Screen 
           name="Rules"
--- Big2/src/screens/Rules.js
+++ Big2/src/screens/Rules.js
@@ -3,12 +3,8 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const Rules = ({ navigation, route }) => {
 
-  const { title, date } = route.params
-
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{title}</Text>
-      <Text>{date}</Text>
     </View>
   )
 }

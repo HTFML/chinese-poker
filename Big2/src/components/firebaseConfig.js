d1374dfa6744cda1a3f356e01bdbf7657e8a7494
Brett Saxon <brettcsaxon@gmail.com>
2020-04-24 17:28:09 -0400

    splash update

---
+++ Big2/src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdT7C9QK21_S5ZicjNmlpuKVlfsg81tPg",
+  authDomain: "chinese-poker-670ac.firebaseapp.com",
+  databaseURL: "https://chinese-poker-670ac.firebaseio.com",
+  projectId: "chinese-poker-670ac",
+  storageBucket: "chinese-poker-670ac.appspot.com",
+  messagingSenderId: "666812993050",
+  appId: "1:666812993050:web:7754ae63c4e67b80860ba3",
+  measurementId: "G-YZZ5YVWN7D"
+};
+
+export default firebaseConfig

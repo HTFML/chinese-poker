d5d4d0a36edffd5b7eab4be520144da4b60c271c
Oneil <oneilswilliams@gmail.com>
2020-04-29 15:49:47 -0400

    undeer construction

--- Big2/src/screens/PasswordChangeScreen.js
+++ Big2/src/screens/PasswordChangeScreen.js
@@ -1,46 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, Button, Alert} from 'react-native';
 
-const PasswordChangeScreen = ({ route }) => {
+const PasswordChangeScreen = ({route}) => {
 
   const { user } = route.params
+  const [
+    value, 
+    submittedPassword
+  ] = useState("")
 
-  return(
-    <View style={styles.mainContainer}>
-      <TextInput style = {styles.input}
-        placeholder = "Current Password"
-        autoCapitalize = "none"
-      />
-      <TextInput style = {styles.input}
-        placeholder = "New Password"
-        autoCapitalize = "none"
-      />
-      <TextInput style = {styles.input}
-        placeholder = "New Password, again"
-        autoCapitalize = "none"
-      />
-      <Button title="Submit"
-        onPress={()=>Alert.alert("Password Changed")}
-      />
-      <Text>{user.password}</Text>
-      
-    </View>
-  )
+  const confirmedPassword = () => {       
+    if (submittedPassword==="hello"){
+      return Alert.alert("Correct Password!")
+    }
+    else {return Alert.alert("Try again")}
+  }
+
+    return(
+      <View 
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 23
+      }}
+      >
+        <TextInput 
+          style = {{
+            margin: 15,
+            height: 40,
+            borderWidth: 1,
+            width: 200}}
+          placeholder = "Current Password"
+          autoCapitalize = "none"
+          onChangeText={text=>submittedPassword(text)}
+          value={value}
+        />
+        {/* <TextInput style = {styles.input}
+          placeholder = "New Password"
+          autoCapitalize = "none"
+        />
+        <TextInput style = {styles.input}
+          placeholder = "New Password, again"
+          autoCapitalize = "none"
+        /> */}
+        <Button title="Submit"
+          onPress={confirmedPassword}
+        />
+        <Text>{user.password}</Text>
+        
+      </View>
+    )
+
+//   const styles = StyleSheet.create({
+  
+//     mainContainer: {
+//     flex: 1,
+//     alignItems: 'center',
+//     paddingTop: 23
+//   },
+//   input: {
+//     margin: 15,
+//     height: 40,
+//     borderWidth: 1,
+//     width: 200
+//  }
+// })
 }
 
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    alignItems: 'center',
-    paddingTop: 23
-  },
-  input: {
-    margin: 15,
-    height: 40,
-    borderWidth: 1,
-    width: 200
- }
-})
+
 
 
 export default PasswordChangeScreen

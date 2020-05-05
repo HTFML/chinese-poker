7cae1a6993a0299a5cf781e603962604bff5dff7
Oneil <oneilswilliams@gmail.com>
2020-05-05 15:34:52 -0400

    settings

--- Big2/src/screens/AudioScreen.js
+++ Big2/src/screens/AudioScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Switch, Text, StyleSheet } from "react-native";
+import { View, Switch, Text, StyleSheet, Alert } from "react-native";
+import Sound from 'react-native-sound'
 
 const AudioScreen = () => {
 
@@ -9,31 +10,44 @@ const AudioScreen = () => {
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   const toggleSwitchFX = () => setIsSFXOn(previousState => !previousState);
 
+  const templeSound = new Sound('temple_morning.mp3')
+
+  componentDidMount = () => {
+    templeSound.play()
+  }
+
   return (
-      <View style={styles.mainContainer}>
+    <View style={styles.mainContainer}>
+      <View style={{flexDirection: 'row'}}>
         <Text >Background Music</Text>
         <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          trackColor={{ false: "#767577", true: "#FF0000" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#FFFFFF"}
           ios_backgroundColor="#3e3e3e"
           onValueChange={toggleSwitch}
           value={isEnabled}
         />
+      </View>
+
+      <View style={{flexDirection: 'row'}}>
         <Text>Sound Effects</Text>
         <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isSFXOn ? "#f5dd4b" : "#f4f3f4"}
+          trackColor={{ false: "#767577", true: "#FF0000" }}
+          thumbColor={isSFXOn ? "#FFFF00" : "#FFFFFF"}
           ios_backgroundColor="#3e3e3e"
           onValueChange={toggleSwitchFX}
           value={isSFXOn}
         />
       </View>
+    </View>
   )
 }
+
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 2,
+    padding: "5%",
     // justifyContent: 'center',
     // alignItems: 'center',
   },

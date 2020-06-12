a20872d5006418e469651bdb96dd3fcd18a1b216
Jacky Feng <jackyfeng530@yahoo.com>
2020-06-11 21:37:14 -0400

    minor updates

--- Big2/src/screens/ChangeScreen.js
+++ Big2/src/screens/ChangeScreen.js
@@ -6,8 +6,6 @@ import { colors } from '../utils/Theme';
 import firebase from '../utils/firebaseConfig';
 import '@firebase/firestore';
 
-const width = Dimensions.get('window').width
-
 const ChangeScreen = ({ route, navigation }) => {
 
   const { title } = route.params
@@ -17,19 +15,21 @@ const ChangeScreen = ({ route, navigation }) => {
   const [ newPW, setNewPW ] = useState("")
   const [ confirmPW, setConfirmPW ] = useState("")
 
-  useLayoutEffect(() => {navigation.setOptions({title: title})}, [navigation, title])
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: title })
+  }, [navigation, title])
 
   const changeUsername = () => {
-    if(newUserName===""){
+    if (newUserName === "") {
       Alert.alert("Field must not be empty")
     } else {
-    currentUser.updateProfile({displayName: newUserName})
+    currentUser.updateProfile({ displayName: newUserName })
     Alert.alert("Username Successfully Changed!")
     navigation.navigate("Settings")
   }}
 
   const changePassword = () => {
-    if(originalPW === "" || newPW === "" || confirmPW === ""){
+    if (originalPW === "" || newPW === "" || confirmPW === "") {
       Alert.alert("Please don't leave any fields blank")
     } else {
     let credential = firebase.auth.EmailAuthProvider.credential(
@@ -37,74 +37,71 @@ const ChangeScreen = ({ route, navigation }) => {
       originalPW
     )
     currentUser.reauthenticateWithCredential(credential)
-    .then(() => {
-      console.log("ReAuthenticated")
-      matchPasswords()
-    })
-    .catch(() => {Alert.alert("Please enter your original password correctly")})
+      .then(() => {
+        console.log("ReAuthenticated")
+        matchPasswords()
+      })
+      .catch(() => {
+        Alert.alert("Please enter your original password correctly")
+      })
     }
   }
 
   const matchPasswords = () => {
-    if (newPW===confirmPW){
+    if (newPW === confirmPW) {
       console.log("Same Passwords")
       currentUser.updatePassword(newPW)
-      .then(() => 
-        {Alert.alert("Password update successful!")
-        navigation.navigate('Settings')
-      })
-      .catch(error => {Alert.alert("An error occurred when changing passwords:", error)})
-    } else return Alert.alert("Passwords entered do not match")
+        .then(() => {
+          Alert.alert("Password update successful!")
+          navigation.navigate('Settings')
+        })
+        .catch(error => {Alert.alert("An error occurred when changing passwords:", error)})
+    } else Alert.alert("Passwords entered do not match")
   }
 
-  if (title=="Change Password") {
-    return(
-      <View style={styles.container}> 
+  if (title == "Change Password") {
+    return (
+      <View style={styles.container}>
         <TextInput
-          style={styles.input}
           placeholder="Enter Orginal Password"
-          onChangeText={text=>setOrginialPW(text)}
+          onChangeText={text => setOrginialPW(text)}
           value={originalPW}
           autoCapitalize="none"
           textInputProps={{ secureTextEntry: true }}
         />
         <TextInput
-          style={styles.input}
           placeholder="New Password"
-          onChangeText={text=>setNewPW(text)}
+          onChangeText={text => setNewPW(text)}
           value={newPW}
           autoCapitalize="none"
           textInputProps={{ secureTextEntry: true }}          
         />
         <TextInput
-          style={styles.input}
           placeholder="Confirm New Password"
-          onChangeText={text=>setConfirmPW(text)}
+          onChangeText={text => setConfirmPW(text)}
           value={confirmPW}
           autoCapitalize="none"
           textInputProps={{ secureTextEntry: true }}
         />
         <Button
           title="SUBMIT" 
-          onPress={()=>changePassword()}
-          width={width-50}
+          onPress={() => changePassword()}
           margin={15}
         />
       </View>
     )
-  } else if (title=="Change Username"){
+  } else if (title == "Change Username") {
     return (
       <View style={styles.container}>
         <TextInput
           placeholder="Enter New Username"
-          onChangeText={text=>setNewUsername(text)}
+          onChangeText={text => setNewUsername(text)}
           value={newUserName}
           autoCapitalize="none"
         />
         <Button
           title="SUBMIT" 
-          onPress={()=>changeUsername()}
-          width={width-50}
+          onPress={ ()=> changeUsername()}
           margin={15}
         />
       </View>
@@ -115,11 +112,10 @@ const ChangeScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: 'center',
+    alignItems: 'center',
     backgroundColor: colors.red,
     justifyContent: 'center'
-  }
+  },
 })
-
 
 export default ChangeScreen
